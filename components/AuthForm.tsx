58c077c4f068f4439c5fae4c2e216db2f03badc9
aniketocto <aniketocto@gmail.com>
2024-05-05 00:01:50 +0530

    Sign In Page

---
+++ components/AuthForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import CustomField from "./Customfields";
+import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+
+const AuthForm = ({ type }: { type: string }) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+    console.log(values);
+    setIsLoading(false);
+  }
+
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="flex cursor-pointer items-center gap-1">
+          <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon" />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Horizon
+          </h1>
+        </Link>
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className=" text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          <p className="text-16 font-normal text-gray-600">
+            {user ? "Link YOur account to get started" : "Please enter details"}
+          </p>
+        </div>
+      </header>
+
+      {user ? (
+        <div className="flex flex-col gap-4">{/*Plaid Accoutt*/}</div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <CustomField
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your email"
+              />
+              <CustomField
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
+
+              <Button type="submit" className="form-btn" disabled={isLoading}>
+                {isLoading ? (
+                  <>
+                    <Loader2 size={20} className="animate-spin" />
+                    &nbsp; Loading...
+                  </>
+                ) : type === "sign-in" ? (
+                  "Sign In"
+                ) : (
+                  "Sign Up"
+                )}
+              </Button>
+            </form>
+          </Form>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default AuthForm;
