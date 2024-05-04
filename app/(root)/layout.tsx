7b917034eb3def363ab233b19e32f9d231074fd4
aniketocto <aniketocto@gmail.com>
2024-05-04 19:34:43 +0530

    Sidebar|Nav|RightSideBar|Routing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,27 @@
+import Mobilenav from "@/components/Mobilenav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Aniket", lastName: "Khambal" };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <Mobilenav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import Headerbox from "@/components/Headerbox";
+import RightsideBar from "@/components/RightsideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Aniket",
+    lastName: "Khambal",
+    email: "aniketocto@gmail.com",
+  };
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <Headerbox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
 
-export default Home
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={5550.36}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightsideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 750 }, { currentBalance: 1000.25 }]}
+      />
+    </div>
+  );
+};
+
+export default Home;
